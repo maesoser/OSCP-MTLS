41fe9ecfadf66b1e0890778798e771e874b33938
Sergio <massesos@gmail.com>
2024-03-07 15:50:40 +0000

    Refactor index.js
    Refactor the code in order to have the MTLS Validator as a separate class. This change simplies the process of incorporating extra modifications to the request in addition to OCSP Validation.

--- index.js
+++ index.js
@@ -11,116 +11,130 @@
 
 import * as asn1js from 'asn1js'
 import { getRandomValues, Certificate, Extension, OCSPRequest, OCSPResponse,  GeneralName, BasicOCSPResponse } from 'pkijs';
-export default {
-  async fetch(request, env, ctx) {
-    
-    //https://gist.github.com/robincher/5c73e8ccb53fad9b611778ab363a416a
-    
-    function base64StringToArrayBuffer(b64str) {
-      let byteStr = atob(b64str);
-      let bytes = new Uint8Array(byteStr.length);
-      for (let i = 0; i < byteStr.length; i++) {
-        bytes[i] = byteStr.charCodeAt(i);
-      }
-      return bytes.buffer;
+
+export class MTLSValidator {
+  constructor(clientCA, rootCA) {
+    this.clientCA = this.buildCertificate(this.b64stringToArrayBuffer(clientCA));
+    this.rootCA = this.buildCertificate(this.b64stringToArrayBuffer(rootCA));
+  }
+  
+  b64stringToArrayBuffer(b64str) {
+    let byteStr = atob(b64str);
+    let bytes = new Uint8Array(byteStr.length);
+    for (let i = 0; i < byteStr.length; i++) {
+      bytes[i] = byteStr.charCodeAt(i);
     }
-    
-    function printCertificate (certificateBuffer) {
-      let asn1 = asn1js.fromBER(certificateBuffer);
-      if(asn1.offset === (-1)) {
-        console.log("Can not parse binary data");
-      } 
-      return new Certificate({ schema: asn1.result });
-    }
-  
-  // Prepare Certs
-  
-  const b64Cl = request.headers.get('cf-client-cert-der-base64');
-  const berCl = base64StringToArrayBuffer(b64Cl);
-  const certCl = printCertificate(berCl);
-  
-  const b64Is = `${env.CA_CLIENT_ISSUER}`;
-  const berIs = base64StringToArrayBuffer(b64Is);
-  const certIs = printCertificate(berIs);
-  
-  const b64Ro = `${env.CA_OCSP_ROOT}`;
-  const berRo = base64StringToArrayBuffer(b64Ro);
-  const certRo = printCertificate(berRo);
-  
-  // Create OCSP request
-  //https://pkijs.org/docs/classes/OCSPRequest.html
+    return bytes.buffer;
+  }    
 
-  const ocspReq = new OCSPRequest();
-  ocspReq.tbsRequest.requestorName = new GeneralName({
-    type: 4,
-    value: certCl.subject,
-  });
-  
-  await ocspReq.createForCertificate(certCl, {
-    hashAlgorithm: "SHA-256",
-    issuerCertificate: certIs,
-  });
-  
-  const nonce = getRandomValues(new Uint8Array(10));
-  ocspReq.tbsRequest.requestExtensions = [
-    new Extension({
-      extnID: "1.3.6.1.5.5.7.48.1.2", // nonce
-      extnValue: new asn1js.OctetString({ valueHex: nonce.buffer }).toBER(),
-    })
-  ];
-  
-  // Encode OCSP request
-  
-  const ocspReqRaw = ocspReq.toSchema(true).toBER();
-  
-  // Get OCSP responder URL
-  
-  const extAIA = certCl.extensions.find((extension) => extension.extnID === '1.3.6.1.5.5.7.1.1');
-  const parsedOcspValue = extAIA.parsedValue.accessDescriptions.find((parsedValue) => parsedValue.accessMethod === '1.3.6.1.5.5.7.48.1');
-  const ocspUrl = parsedOcspValue.accessLocation.value;
-
-  const oRequest = new Request(ocspUrl, { method: 'POST', body: ocspReqRaw });
-  oRequest.headers.set('content-type', 'application/ocsp-request');
-  const oResponce = await fetch(oRequest);
-
-  // Parse OCSP response
-
-  const ocspRespRaw = await oResponce.arrayBuffer();
-  const asnOcspResp = asn1js.fromBER(ocspRespRaw);
-  const ocspResp = new OCSPResponse({ schema: asnOcspResp.result });
-  if (!ocspResp.responseBytes) {
-    throw new Error("No \"ResponseBytes\" in the OCSP Response - nothing to verify");
+  buildCertificate(certificateBuffer) {
+    let asn1 = asn1js.fromBER(certificateBuffer);
+    if(asn1.offset === (-1)) {
+      throw new Error("Cannot parse binary data - CAs not correctly loaded");
+      return null
+    } 
+    return new Certificate({ schema: asn1.result });
   }
 
-  // Check certificate status
+  getOCSPUrl(){
+    const extAIA = this.clientCert.extensions.find((extension) => extension.extnID === '1.3.6.1.5.5.7.1.1');
+    const parsedOcspValue = extAIA.parsedValue.accessDescriptions.find((parsedValue) => parsedValue.accessMethod === '1.3.6.1.5.5.7.48.1');
+    return parsedOcspValue.accessLocation.value;
+  }
 
-  const certstatus = await ocspResp.getCertificateStatus(certCl, certIs);
-  console.log(certstatus.status);
-
-  // Varidate OCSP responder
   //https://pkijs.org/docs/classes/ResponseData.html#responses
+  async validateOCSPResponse(OCSPResponse){
+          
+    const asnOcspRespBasic = asn1js.fromBER(OCSPResponse.responseBytes.response.valueBlock.valueHex);
+    const ocspBasicResp = new BasicOCSPResponse({ schema: asnOcspRespBasic.result });
+    
+    const ok = await ocspBasicResp.verify({ trustedCerts: [this.rootCA] });
+    return ok
+  }
+
+  async doOCSPRequest(){
+    const ocspReq = new OCSPRequest();
+    ocspReq.tbsRequest.requestorName = new GeneralName({ type: 4, value: this.clientCert.subject, });
+    
+    await ocspReq.createForCertificate(this.clientCert, { hashAlgorithm: "SHA-256", issuerCertificate: this.clientCA, });
+    
+    const nonce = getRandomValues(new Uint8Array(10));
+    ocspReq.tbsRequest.requestExtensions = [
+      new Extension({
+        extnID: "1.3.6.1.5.5.7.48.1.2", // nonce
+        extnValue: new asn1js.OctetString({ valueHex: nonce.buffer }).toBER(),
+      })
+    ];
+    // Encode OCSP request
+    const ocspReqRaw = ocspReq.toSchema(true).toBER();
+    
+    const ocspUrl = this.getOCSPUrl()
+
+    const oRequest = new Request(ocspUrl, { method: 'POST', body: ocspReqRaw });
+    oRequest.headers.set('content-type', 'application/ocsp-request');
+    const oResponce = await fetch(oRequest);
   
-  const asnOcspRespBasic = asn1js.fromBER(ocspResp.responseBytes.response.valueBlock.valueHex);
-  const ocspBasicResp = new BasicOCSPResponse({ schema: asnOcspRespBasic.result });
-  
-  const ok = await ocspBasicResp.verify({ trustedCerts: [certRo] });
-  console.log(ok);
-  
-  // Send response
-  //https://datatracker.ietf.org/doc/html/rfc6960
-  const oStat = certstatus.status;
-    switch (oStat) {
-    case 0:
-      return new Response("OCSP Good");
-      break;
-    case 1:
-      return new Response("OCSP Revoked");
-      break;
-    case 2:
-      return new Response("OCSP Unknown");
-      break;
-    default:
-      return new Response("Bad");
+    // Parse OCSP response
+    const ocspRespRaw = await oResponce.arrayBuffer();
+    const asnOcspResp = asn1js.fromBER(ocspRespRaw);
+    const ocspResp = new OCSPResponse({ schema: asnOcspResp.result });
+    if (!ocspResp.responseBytes) {
+      throw new Error("Empty OCSP Response - nothing to verify");
+    }
+    return ocspResp
+  }
+
+  async getCertificateStatus(clientCertificate){
+    this.clientCert = this.buildCertificate(this.b64stringToArrayBuffer(clientCertificate));
+    const OCSPResponse = await this.doOCSPRequest();
+    const ResponseIsValidated = this.validateOCSPResponse(OCSPResponse);
+    if (!ResponseIsValidated){
+      throw new Error("OCSP Response is not valid");
+    }
+    const certstatus = await ocspResp.getCertificateStatus(this.clientCert, this.clientCA);
+    return certstatus.status;
+  }
+
+  async isRevoked(clientCertificate){
+    //https://datatracker.ietf.org/doc/html/rfc6960
+    // 0 is Good
+    // 1 is Revoked
+    // 2 is Unknown
+    const status = await this.getCertificateStatus(clientCertificate);
+    console.log(status);
+    if (status === 1) return true;
+    return false;
+  }
+}
+
+export default {
+
+  async fetch(request, env, ctx) {
+    try{
+      let mTLSValidator = new MTLSValidator(`${env.CA_CLIENT_ISSUER}`,`${env.CA_OCSP_ROOT}`)
+      
+      const b64ClientCert = request.headers.get('cf-client-cert-der-base64') || null;
+
+      if (b64ClientCert == null){
+        return new Response(JSON.stringify({ error: "cert_not_found" }), { status:400 })
+      }
+      
+      /* Simpler version
+      let isRevoked = await mTLSValidator.IsRevoked(b64ClientCert);
+      if (isRevoked === true){
+        return new Response({ error: "cert_revoked" }, { status:400 })
+      }
+      */
+      const status = await mTLSValidator.getCertificateStatus(b64ClientCert)
+      let newRequest = new Request(request);
+      newRequest.headers.set("OCSP-Processed","True")
+      newRequest.headers.set("OCSP-Status",status)
+      newRequest.headers.set("OCSP-Revoked",status === 1 ? "True" : "False")
+      return await fetch(newRequest)
+
+    }catch(e){
+      return new Response(JSON.stringify({ error: e.message }), { status: 500 })
     }
   }
-};
+}
+
